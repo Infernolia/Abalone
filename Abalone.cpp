55cdd8e5cf63fa01d68907292d996e207acfbcfb
mufaddal12 <47853019+mufaddal12@users.noreply.github.com>
2019-07-24 20:10:30 +0530

    Initial commit

---
+++ Abalone.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class abalone
+{
+	vector<vector<int>> board;
+
+public:
+	abalone();
+	void display();
+	void disp();
+};
+
+abalone::abalone()
+{
+	board = { //more changes
+				{  0,  0,  0,  0,  0},
+
+			  {  0,  0,  0,  0,  0,  0},
+
+			{ -1, -1,  0,  0,  0, -1, -1},
+
+		  { -1, -1, -1, -1, -1, -1, -1, -1},
+
+		{ -1, -1, -1, -1, -1, -1, -1, -1, -1},
+
+		  { -1, -1, -1, -1, -1, -1, -1, -1},
+
+			{ -1, -1,  1,  1,  1, -1, -1},
+
+			  {  1,  1,  1,  1,  1,  1},
+
+				{  1,  1,  1,  1,  1}
+	};
+}
+
+void abalone::display()
+{
+	char row = 'A';
+	int dia = 9;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 3 - i; j >= 0; j--)
+		{
+			cout << " ";
+		}
+		cout << row << " ";
+		for (int j = 0; j < board[i].size(); j++)
+		{
+			if (board[i][j] == -1) cout << "+ ";
+			else if (board[i][j] == 0) cout << "O ";
+			else cout << "@ ";
+		}
+		cout << endl;
+		row++;
+	}
+	for (int i = 5; i < 9; i++)
+	{
+		for (int j = 0; j < i - 4; j++)
+		{
+			cout << " ";
+		}
+		cout << row << " ";
+		for (int j = 0; j < board[i].size(); j++)
+		{
+			if (board[i][j] == -1) cout << "+ ";
+			else if (board[i][j] == 0) cout << "O ";
+			else cout << "@ ";
+		}
+		cout << dia << endl;
+		row++;
+		dia--;
+	}
+	for (int i = 0; i < 7; i++) cout << " ";
+	for (int i = 0; i < 5; i++, dia--) cout << 6 - dia << " ";
+	cout << endl;
+}
+
+void abalone::disp()
+{
+	for (int i = 0; i < board.size(); i++)
+	{
+		for (int j = 0; j < board[i].size(); j++)
+			cout << board[i][j] << " ";
+		cout << endl;
+	}
+}
+int main()
+{
+	abalone a;
+	a.display();
+	a.disp();
+	system("pause");
+}
